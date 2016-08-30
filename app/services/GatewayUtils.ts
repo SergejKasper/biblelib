d7c61487043ea1667601c653b85f5b13a6ba4f37
Sergej Kasper <sergejkasper@gmail.com>
2016-08-30 09:29:20 +0200

    finallize app/web socket-interaction

--- app/services/GatewayUtils.ts
+++ app/services/GatewayUtils.ts
@@ -25,16 +25,16 @@ export class GatewayUtils {
   public borrowBook(isbn: string, borrower:Borrower, cover?:string): any {
     let data:any = {isbn: isbn, action: "BORROW", borrower: borrower};
     if(cover) data.cover = cover;
-    return this.http.post(this.interactionUrl, data, interactionOpts);
+    return this.http.post(this.interactionUrl, data, this.interactionOpts);
   }
   public returnBook(isbn: string, borrower:Borrower){
     let data:any = {isbn: isbn, action: "RETURN", borrower: borrower};
-    return this.http.post(this.interactionUrl, data, interactionOpts);
+    return this.http.post(this.interactionUrl, data, this.interactionOpts);
   }
   public addBook(isbn: string, cover?:string):Observable<Response>{
     let data:any = {isbn: isbn, action: "NEW"};
     if(cover) data.cover = cover;
-    return this.http.post(this.interactionUrl, data, interactionOpts);
+    return this.http.post(this.interactionUrl, data, this.interactionOpts);
   }
   /*public getBorrowers(): Observable<Borrower[]> {
   return this.http.get(this.borrowerUrl).map(this.extractData)
